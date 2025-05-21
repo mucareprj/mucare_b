a9b2e25c6301d038465fa2a145f20833939ce83c
dongwon <adsdoctor@naver.com>
2025-05-22 02:14:51 +0900

    250522 수정

---
+++ src/main/java/mucare/prj/config/WebConfig.java
@@ -0,0 +1,17 @@
+package mucare.prj.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        // 정적 리소스 핸들링 제외
+        registry
+            .addResourceHandler("/api/**")
+            .addResourceLocations("classpath:/nothing/");
+    }
+}
