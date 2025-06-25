e67cf4ab999c54c2f0b78119f0772ad77c2e59b7
Yeomjihye <jhlol319@gmail.com>
2025-06-25 17:40:54 +0900

    commit
    25.06.25

--- src/main/java/mucare/prj/mapper/UserMapper.java
+++ src/main/java/mucare/prj/mapper/UserMapper.java
@@ -6,6 +6,7 @@ import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Param;
 
 import mucare.prj.domain.User;
+import mucare.prj.domain.UserAgreement;
 
 @Mapper
 public interface UserMapper {
@@ -15,5 +16,8 @@ public interface UserMapper {
     int updateLastLogin(String email);
     User findByProvider(Map<String, Object> params);
     int updateLastLoginByProvider(@Param("provider") String provider, @Param("providerId") String providerId);
+
+    // 약관동의
+    int insertAgreement(UserAgreement agreement);
     
 }
